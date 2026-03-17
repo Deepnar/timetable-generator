5573a6ca68de6314a1c4da5f4e918e1edf5a5b0c
Deepesh Sonar <18deepnar@gmail.com>
2026-03-18 00:06:44 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from database import get_db, engine
+import models, schemas
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/rooms", response_model=list[schemas.RoomResponse])
+def get_rooms(db: Session = Depends(get_db)):
+    rooms = db.scalars(select(models.Room).where(models.Room.is_active == True)).all()
+    return rooms
+
+@app.get("/rooms/{id}", response_model=schemas.RoomResponse)
+def get_room(id: int, db: Session = Depends(get_db)):
+    room = db.scalars(select(models.Room).where(models.Room.id == id)).first()
+    if not room:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Room with id {id} not found")
+    return room
+
+@app.post("/rooms", status_code=status.HTTP_201_CREATED,
+          response_model=schemas.RoomResponse)
+def create_room(room: schemas.RoomCreate, db: Session = Depends(get_db)):
+    new_room = models.Room(**room.model_dump())
+    db.add(new_room)
+    db.commit()
+    db.refresh(new_room)
+    return new_room
+
+@app.delete("/rooms/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_room(id: int, db: Session = Depends(get_db)):
+    room = db.scalars(select(models.Room).where(models.Room.id == id)).first()
+    if not room:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Room with id {id} not found")
+    room.is_active = False
+    db.commit()
+    return
+
+@app.put("/rooms/{id}", response_model=schemas.RoomResponse)
+def update_room(id: int, updated_room: schemas.RoomCreate,
+                db: Session = Depends(get_db)):
+    room = db.scalars(select(models.Room).where(models.Room.id == id)).first()
+    if not room:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Room with id {id} not found")
+    for key, value in updated_room.model_dump().items():
+        setattr(room, key, value)
+    db.commit()
+    db.refresh(room)
+    return room
